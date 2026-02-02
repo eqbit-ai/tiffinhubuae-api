dde1c76789ad0f8ba25fa52e6697b53fc6e67e41
ashutosh <ashuken266@gmail.com>
2026-02-02 16:45:55 +0530

    Add missing seasonal and ramadan fields migration

---
+++ prisma/migrations/20260202120100_add_seasonal_ramadan_fields/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN "seasonal_mode" TEXT;
+ALTER TABLE "User" ADD COLUMN "seasonal_mode_start" TEXT;
+ALTER TABLE "User" ADD COLUMN "seasonal_mode_end" TEXT;
+ALTER TABLE "User" ADD COLUMN "ramadan_lunch_time" TEXT;
+ALTER TABLE "User" ADD COLUMN "ramadan_dinner_time" TEXT;
+ALTER TABLE "User" ADD COLUMN "ramadan_suhoor_time" TEXT;
+ALTER TABLE "User" ADD COLUMN "ramadan_iftar_time" TEXT;
